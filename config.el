ea0b802d50b1946852e962dd5f339968da12dac6
Ting Zhou <zhouting@umich.edu>
2018-10-02 23:53:55 -0700

    remove bydra/ref keybinding

--- config.el
+++ config.el
@@ -158,31 +158,6 @@
          "k" #'lsp-ui-peek--select-prev
          "l" #'lsp-ui-peek--select-next-file
          )
-
-   (defhydra hydra/ref (evil-normal-state-map "x")
-     "reference"
-     ("d" lsp-ui-peek-find-definitions "next" :bind nil)
-     ("n" (-let [(i . n) (lsp-ui-find-next-reference)]
-            (if (> n 0) (message "%d/%d" i n))) "next")
-     ("p" (-let [(i . n) (lsp-ui-find-prev-reference)]
-            (if (> n 0) (message "%d/%d" i n))) "prev")
-     ("R" (-let [(i . n) (lsp-ui-find-prev-reference
-                          (lambda (x)
-                            (/= (logand (gethash "role" x 0) 8) 0)))]
-            (if (> n 0) (message "read %d/%d" i n))) "prev read" :bind nil)
-     ("r" (-let [(i . n) (lsp-ui-find-next-reference
-                          (lambda (x)
-                            (/= (logand (gethash "role" x 0) 8) 0)))]
-            (if (> n 0) (message "read %d/%d" i n))) "next read" :bind nil)
-     ("W" (-let [(i . n) (lsp-ui-find-prev-reference
-                          (lambda (x)
-                            (/= (logand (gethash "role" x 0) 16) 0)))]
-            (if (> n 0) (message "write %d/%d" i n))) "prev write" :bind nil)
-     ("w" (-let [(i . n) (lsp-ui-find-next-reference
-                          (lambda (x)
-                            (/= (logand (gethash "role" x 0) 16) 0)))]
-            (if (> n 0) (message "write %d/%d" i n))) "next write" :bind nil)
-     )
    )
 
 (setq magit-repository-directories '(("~/Dev" . 2)))
