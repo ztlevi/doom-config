24238a4a39b8b0f23e00fa85646b4475de7e1c2d
Ting Zhou <zhouting@umich.edu>
2018-10-03 15:03:11 -0700

    remove unnecessary xref keys

--- config.el
+++ config.el
@@ -63,52 +63,6 @@
     )
   )
 
-(defun +advice/xref-set-jump (&rest args)
-  (lsp-ui-peek--with-evil-jumps (evil-set-jump)))
-(advice-add '+lookup/definition :before #'+advice/xref-set-jump)
-(advice-add '+lookup/references :before #'+advice/xref-set-jump)
-
-
-(defvar +my/xref-blacklist nil
-  "List of paths that should not enable xref-find-* or dumb-jump-go")
-
-;;; Override
-;; This function is transitively called by xref-find-{definitions,references,apropos}
-(after! xref
-  ;; This is required to make `xref-find-references' not give a prompt.
-  ;; `xref-find-references' asks the identifier (which has no text property)
-  ;; and then passes it to `lsp-mode', which requires the text property at
-  ;; point to locate the references.
-  ;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=29619
-  (setq xref-prompt-for-identifier '(not xref-find-definitions
-                                         xref-find-definitions-other-window
-                                         xref-find-definitions-other-frame
-                                         xref-find-references))
-
-  (defun xref--show-xrefs (xrefs display-action &optional always-show-list)
-    ;; PATCH
-    (lsp-ui-peek--with-evil-jumps (evil-set-jump))
-
-    ;; PATCH Jump to the first candidate
-    (if (not (cdr xrefs))
-        (xref--pop-to-location (car xrefs) display-action)
-      (funcall xref-show-xrefs-function xrefs
-               `((window . ,(selected-window))))
-      ))
-  )
-
-(after! ivy-xref
-  ;; (defun ivy-xref-show-xrefs (xrefs alist)
-  ;;   (minibuffer-with-setup-hook #'hydra-ivy/body
-  ;;      (minibuffer-with-setup-hook #'ivy-toggle-calling
-  ;;        (ivy-read "xref: " (ivy-xref-make-collection xrefs)
-  ;;                  :require-match t
-  ;;                  :action #'(lambda (candidate)
-  ;;                              (xref--show-location (cdr candidate) 'quit))))))
-  ;; (push '(xref-find-references) ivy-display-functions-alist)
-  (push '(ivy-xref-show-xrefs . nil) ivy-sort-functions-alist)
-  )
-
 (def-package! rust-mode
   :mode "\\.rs$"
   :config
