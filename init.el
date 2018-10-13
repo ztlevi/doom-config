193928ed76a45908dc75d040809884fd6bd91b19
Ting Zhou <zhouting@umich.edu>
2018-10-13 14:03:33 -0700

    remove rss

--- init.el
+++ init.el
@@ -132,7 +132,7 @@
        :app
       ;(email +gmail)    ; emacs as an email client
       ;irc               ; how neckbeards socialize
-       (rss +org)        ; emacs as an RSS reader
+      ;(rss +org)        ; emacs as an RSS reader
       ;twitter           ; twitter client https://twitter.com/vnought
        (write            ; emacs as a word processor (latex + org + markdown)
         +wordnut         ; wordnet (wn) search
