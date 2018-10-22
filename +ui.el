4f457bc0ba4db4421c36aa6a7cc0e3294eb2116e
Ting Zhou <zhouting@umich.edu>
2018-10-22 16:47:32 -0700

    update mac modeline height

--- +ui.el
+++ +ui.el
@@ -3,7 +3,7 @@
 (cond (IS-MAC
        (setq doom-font (font-spec :family "Operator Mono Lig" :size 16)
              doom-big-font (font-spec :family "Operator Mono Lig" :size 22)
-             +modeline-height 28))
+             +modeline-height 32))
       (IS-LINUX
        (setq doom-font (font-spec :family "Ubuntu Mono" :size 36)
              doom-big-font (font-spec :family "Ubuntu Mono" :size 40)
