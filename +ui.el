907a138ff0fcc16ff63527243b19d6cf0534dd21
Ting Zhou <zhouting@umich.edu>
2018-10-22 01:18:23 -0700

    update ui setting

--- +ui.el
+++ +ui.el
@@ -1,7 +1,13 @@
 ;;; private/my/+ui.el -*- lexical-binding: t; -*-
 
-(setq doom-font (font-spec :family "Operator Mono Lig" :size 16))
-(setq doom-big-font (font-spec :family "Operator Mono Lig" :size 22))
+(cond (IS-MAC
+       (progn
+         (setq doom-font (font-spec :family "Operator Mono Lig" :size 16))
+         (setq doom-big-font (font-spec :family "Operator Mono Lig" :size 22))))
+      (IS-LINUX
+       (progn
+         (setq doom-font (font-spec :family "Ubuntu Mono" :size 36))
+         (setq doom-big-font (font-spec :family "Ubuntu Mono" :size 40)))))
 
 (setq +doom-modeline-height 28)
 (setq +doom-modeline-buffer-file-name-style 'relative-to-project)
@@ -35,43 +41,15 @@
    ))
 (add-hook! 'doom-load-theme-hook #'+my/set-faces)
 
-;; enable natural title bar for emacs-plus
-(when IS-EMACS-PLUS
-  (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-  (add-to-list 'default-frame-alist '(ns-appearance . light)))
+(when IS-MAC
+  ;; enable natural title bar for emacs-plus
+  (when IS-EMACS-PLUS
+    (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+    (add-to-list 'default-frame-alist '(ns-appearance . light)))
 
-;; enable ligatures support
-;; details here: https://github.com/tonsky/FiraCode/wiki/Emacs-instructions
-(progn
-  (let ((alist '((33 . ".\\(?:\\(?:==\\|!!\\)\\|[!=]\\)")
-                 (35 . ".\\(?:###\\|##\\|_(\\|[#(?[_{]\\)")
-                 (36 . ".\\(?:>\\)")
-                 (37 . ".\\(?:\\(?:%%\\)\\|%\\)")
-                 (38 . ".\\(?:\\(?:&&\\)\\|&\\)")
-                 (42 . ".\\(?:\\(?:\\*\\*/\\)\\|\\(?:\\*[*/]\\)\\|[*/>]\\)")
-                 (43 . ".\\(?:\\(?:\\+\\+\\)\\|[+>]\\)")
-                 (45 . ".\\(?:\\(?:-[>-]\\|<<\\|>>\\)\\|[<>}~-]\\)")
-                 ;; (46 . ".\\(?:\\(?:\\.[.<]\\)\\|[.=-]\\)")
-                 (47 . ".\\(?:\\(?:\\*\\*\\|//\\|==\\)\\|[*/=>]\\)")
-                 (48 . ".\\(?:x[a-zA-Z]\\)")
-                 (58 . ".\\(?:::\\|[:=]\\)")
-                 (59 . ".\\(?:;;\\|;\\)")
-                 (60 . ".\\(?:\\(?:!--\\)\\|\\(?:~~\\|->\\|\\$>\\|\\*>\\|\\+>\\|--\\|<[<=-]\\|=[<=>]\\||>\\)\\|[*$+~/<=>|-]\\)")
-                 (61 . ".\\(?:\\(?:/=\\|:=\\|<<\\|=[=>]\\|>>\\)\\|[<=>~]\\)")
-                 (62 . ".\\(?:\\(?:=>\\|>[=>-]\\)\\|[=>-]\\)")
-                 (63 . ".\\(?:\\(\\?\\?\\)\\|[:=?]\\)")
-                 (91 . ".\\(?:]\\)")
-                 (92 . ".\\(?:\\(?:\\\\\\\\\\)\\|\\\\\\)")
-                 (94 . ".\\(?:=\\)")
-                 (119 . ".\\(?:ww\\)")
-                 (123 . ".\\(?:-\\)")
-                 (124 . ".\\(?:\\(?:|[=|]\\)\\|[=>|]\\)")
-                 (126 . ".\\(?:~>\\|~~\\|[>=@~-]\\)")
-                 )
-               ))
-    (dolist (char-regexp alist)
-      (set-char-table-range composition-function-table (car char-regexp)
-                            `([,(cdr char-regexp) 0 font-shape-gstring])))))
+  ;; enable ligatures support
+  ;; details here: https://github.com/tonsky/FiraCode/wiki/Emacs-instructions
+  (mac-auto-operator-composition-mode))
 
 (after! ibuffer
   ;; set ibuffer name column width
