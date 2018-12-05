81ebfa5444a655a43a992770fd74ea501f882478
Ting Zhou <zhouting@umich.edu>
2018-12-04 23:41:33 -0800

    cc: temp fix ccls with github recipe

--- modules/private/my-cc/autoload.el
+++ modules/private/my-cc/autoload.el
@@ -6,7 +6,6 @@
 ;;;###autoload
 (defun +ccls//enable ()
   (require 'ccls)
-  (setq-local lsp-ui-sideline-show-symbol nil)
   (condition-case nil
       (lsp-ccls-enable)
     (user-error nil)))
--- modules/private/my-cc/packages.el
+++ modules/private/my-cc/packages.el
@@ -2,5 +2,5 @@
 ;;; private/my-cc/packages.el
 
 (when (executable-find "ccls")
-  (package! ccls))
+  (package! ccls :recipe (:fetcher github :repo "MaskRay/emacs-ccls" :commit "07ad553950e69f862f7c74c9b1f02c00ab450d22")))
 (package! flycheck-google-cpplint :recipe (:fetcher github :repo "flycheck/flycheck-google-cpplint"))
