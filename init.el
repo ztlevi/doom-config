9f28ab151bf51f840820bcdcce2a617a2d21af2a
Ting Zhou <zhouting@umich.edu>
2018-12-25 23:25:04 -0800

    doc: readme update

--- init.el
+++ init.el
@@ -163,9 +163,8 @@
        ;;literate
 
        ;; The default module sets reasonable defaults for Emacs. It also
-       ;; provides a Spacemacs-inspired keybinding scheme, a custom yasnippet
-       ;; library, and additional ex commands for evil-mode. Use it as a
-       ;; reference for your own modules.
+       ;; provides a Spacemacs-inspired keybinding scheme and a smartparens
+       ;; config. Use it as a reference for your own modules.
        (default +bindings +snippets +smartparens)
 
        :private
