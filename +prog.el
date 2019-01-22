76b0651494f1719041662bca8ba756a71d7b00a5
Ting Zhou <zhouting@umich.edu>
2019-01-21 23:39:48 -0800

    prog: lispy use doom's default

--- +prog.el
+++ +prog.el
@@ -165,34 +165,6 @@
 
 (add-hook! 'go-mode-hook (setq indent-tabs-mode nil))
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; LISP
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(def-package! lispy
-  :hook ((common-lisp-mode . lispy-mode)
-         (emacs-lisp-mode . lispy-mode)
-         (scheme-mode . lispy-mode)
-         (racket-mode . lispy-mode)
-         (hy-mode . lispy-mode)
-         (lfe-mode . lispy-mode)
-         (clojure-mode . lispy-mode))
-  :config
-  (add-hook 'lispy-mode-hook #'turn-off-smartparens-mode))
-
-(def-package! lispyville
-  :when (featurep! :feature evil)
-  :hook (lispy-mode . lispyville-mode)
-  :config
-  (lispyville-set-key-theme
-   '(operators
-     c-w
-     (escape insert)
-     (slurp/barf-lispy)
-     additional-movement)))
-
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; LSP
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
