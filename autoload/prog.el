0a9175697fca3a9e9cd617d1336fb6d46e194593
Ting Zhou <zhouting@umich.edu>
2019-01-24 00:22:39 -0800

    prog: add measure-time macro

--- autoload/prog.el
+++ autoload/prog.el
@@ -1,7 +1,13 @@
 ;;; ~/.doom.d/autoload/prog.el -*- lexical-binding: t; -*-
 
-
 ;;;###autoload
 (defun reset-flycheck (&rest _)
   (flycheck-mode -1)
   (flycheck-mode +1))
+
+;;;###autoload
+(defmacro measure-time (&rest body)
+  "Measure the time it takes to evaluate BODY."
+  `(let ((time (current-time)))
+     ,@body
+     (message "%.06f" (float-time (time-since time)))))
