65d694a734e6b1a642d10d547a946a6076d2d7f6
Ting Zhou <zhouting@umich.edu>
2019-03-04 11:34:41 -0800

    ui: update with doom-modeline

--- +ui.el
+++ +ui.el
@@ -6,19 +6,19 @@
   (cond (IS-MAC
          (setq doom-font (font-spec :family "Operator Mono Lig" :size 16)
                doom-big-font (font-spec :family "Operator Mono Lig" :size 22)
-               +modeline-height 32))
+               doom-modeline-height 32))
         (IS-LINUX
          (setq resolution-factor (eval (/ (x-display-pixel-height) 1080.0)))
          (setq doom-font (font-spec :family "Operator Mono" :size (eval (round (* 16 resolution-factor))) :weight 'light)
                doom-big-font (font-spec :family "Operator Mono" :size (eval (round (* 22 resolution-factor))))
-               +modeline-height (eval (round (* 32 resolution-factor))))))
+               doom-modeline-height (eval (round (* 32 resolution-factor))))))
 
   ;; set initl screen size
   (setq initial-frame-alist
         '((width . 110)
           (height . 65))))
 
-(setq +doom-modeline-buffer-file-name-style 'relative-to-project)
+(setq doom-modeline-buffer-file-name-style 'relative-to-project)
 
 (setq +workspaces-on-switch-project-behavior t)
 
