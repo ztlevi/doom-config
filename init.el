51e88cf895d5a0b26a6559407db66575b5b552aa
Ting Zhou <zhouting@umich.edu>
2019-04-17 15:01:20 -0700

    init: add kotlin

--- init.el
+++ init.el
@@ -110,6 +110,7 @@
        java              ; the poster child for carpal tunnel syndrome ;(java +meghanada)
        javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
+       kotlin            ; a better, slicker Java(Script)
        latex             ; writing papers in Emacs has never been so fun
        ;;ledger            ; an accounting system in Emacs
        ;;lua               ; one-based indices? one-based indices
