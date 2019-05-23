5c5e5b7670924612994f7af26cf3abe5a8c143f3
Ting Zhou <zhouting@umich.edu>
2019-05-22 22:06:41 -0700

    init: markdown remove pandoc flag

--- init.el
+++ init.el
@@ -121,8 +121,7 @@
        latex             ; writing papers in Emacs has never been so fun
        ;;ledger            ; an accounting system in Emacs
        ;;lua               ; one-based indices? one-based indices
-      (markdown          ; writing docs for people to ignore
-       +pandoc)          ; pandoc support
+       markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
