52828f32f7660dbf9557cd5ae0b8294ecd6ee75b
Ting Zhou <zhouting@umich.edu>
2019-07-01 11:17:01 -0700

    init: move custom variables to init.el

--- init.el
+++ init.el
@@ -184,3 +184,15 @@
        :private
        ;; lsp-intellij
        )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((conda-project-env-name . "tf")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
