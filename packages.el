61e72abeb579091bc13b1385f43d24ad34e35faf
Ting Zhou <zhouting@umich.edu>
2019-08-28 00:07:47 -0700

    package: straight.el fix recipes

--- packages.el
+++ packages.el
@@ -18,12 +18,12 @@
 (package! link-hint) 
 (package! symbol-overlay) 
 (package! tldr) 
-(package! (blog-admin :recipe (:fetcher github :repo "codefalling/blog-admin"))) 
+(package! blog-admin :recipe (:host github :repo "codefalling/blog-admin"))
 (package! youdao-dictionary) 
 (package! wucuo) 
 (package! grip-mode) 
 (package! org-wild-notifier) 
-(package! (vterm-toggle :recipe (:fetcher github :repo "jixiuf/vterm-toggle"))) 
+(package! vterm-toggle :recipe (:host github :repo "jixiuf/vterm-toggle"))
 
 ;; programming
 (package! import-js) 
@@ -31,4 +31,4 @@
 (package! lsp-python-ms) 
 (package! importmagic) 
 (package! py-isort) 
-(package! (flycheck-google-cpplint :recipe (:fetcher github :repo "flycheck/flycheck-google-cpplint"))) 
+(package! flycheck-google-cpplint :recipe (:host github :repo "flycheck/flycheck-google-cpplint"))
