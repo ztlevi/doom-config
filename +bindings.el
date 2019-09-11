ba3a8c665ad9b7d23f97c8964656176797b9fcce
Ting Zhou <zhouting@umich.edu>
2019-09-11 16:07:36 -0700

    key: distinguish C-i from tab

--- +bindings.el
+++ +bindings.el
@@ -3,6 +3,9 @@
 (when IS-MAC (setq mac-command-modifier 'meta
                    mac-option-modifier  'alt))
 
+;; Distinguish C-i from TAB
+(define-key input-decode-map "\C-i" [C-i])
+
 (map!
  ;; overrides other minor mode keymaps (just for non-evil)
  (:map override ;; general-override-mode-map
@@ -13,6 +16,7 @@
    "C-h m" #'describe-mode
    "M-;"   #'+my/insert-semicolon-at-the-end-of-this-line
    "C-M-;" #'+my/delete-semicolon-at-the-end-of-this-line)
+ "<C-i>"   #'better-jumper-jump-forward
  "M-`"   #'other-frame
  "C-M-o" #'other-frame
  ;; fix OS window/frame navigation/manipulation keys
