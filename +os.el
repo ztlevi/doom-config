41b412e6a4d7d1a23cd52dc8a72f56575346b2d6
Ting Zhou <zhouting@umich.edu>
2019-09-13 11:57:45 -0700

    os: put code first in the shell-apps

--- +os.el
+++ +os.el
@@ -13,7 +13,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Add executable: Clion -> Tools -> Create Command Line Launcher
-(defvar shell-apps '("pycharm" "studio" "clion" "code" "idea")
+(defvar shell-apps '("code" "pycharm" "clion" "idea")
   "Applications collection used for `+shell--open-with' method")
 (defun ivy--read-apps ()
   (ivy-read "Select Applications:" shell-apps))
