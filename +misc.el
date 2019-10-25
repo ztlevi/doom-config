04a088a0d9fed9d7cbe89611f1155743a739369c
Ting Zhou <zhouting@umich.edu>
2019-10-25 11:48:15 -0700

    misc: ignore git commit style checks

--- +misc.el
+++ +misc.el
@@ -176,6 +176,7 @@
 (after! magit
   (setq magit-repository-directories '(("~/Developer" . 2))
         magit-save-repository-buffers nil
+        git-commit-style-convention-checks nil
         magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1)
 
   (magit-wip-after-apply-mode t))
