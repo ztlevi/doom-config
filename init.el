b6add854abc8e198ed1ed179c3f4649007f7e755
Ting Zhou <ztlevi.work@gmail.com>
2020-07-20 00:33:02 -0700

    init: disable pyenv

--- init.el
+++ init.el
@@ -166,7 +166,7 @@
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        (python
-        +pyenv
+        ;; +pyenv
         +conda
         +lsp)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
