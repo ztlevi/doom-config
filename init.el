403d7e3d75e7505a4ab9c82330b7fce04212c7d4
Ting Zhou <ztlevi.work@gmail.com>
2020-08-20 16:58:41 -0700

    init: add tty

--- init.el
+++ init.el
@@ -117,6 +117,10 @@
        tmux              ; an API for interacting with tmux
        upload            ; map local to remote projects via ssh/ftp
 
+       :os
+       (:if IS-MAC macos)  ; improve compatibility with macOS
+       (tty +osc)          ; improve the terminal Emacs experience
+
        :lang
        ;;agda              ; types of types of types of types...
        ;;assembly          ; assembly for fun or debugging
@@ -182,9 +186,6 @@
        ;;terra             ; Earth and Moon in alignment for performance.
        web               ; the tubes
        yaml              ; JSON, but readable
-
-       :os
-       macos             ; MacOS-specific commands
 
        :email
        ;;(mu4e +gmail)
