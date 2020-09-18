7e3523b2575c6fe6df2eb70cba8c9345987c9066
Ting Zhou <ztlevi.work@gmail.com>
2020-09-18 11:33:32 -0700

    misc: add dev as magit scan folder

--- +misc.el
+++ +misc.el
@@ -316,7 +316,7 @@
 
 
 (after! magit
-  (setq magit-repository-directories '(("~/Developer" . 2))
+  (setq magit-repository-directories '(("~/dev" . 2) ("~/Developer" . 2))
         magit-save-repository-buffers nil
         git-commit-style-convention-checks nil
         magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1)
