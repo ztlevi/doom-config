2cacc52913d94255ecb85aa0f210fc5ec6a7c6c1
Ting Zhou <ztlevi.work@gmail.com>
2020-09-30 09:30:23 -0700

    fix window divider

--- +ui.el
+++ +ui.el
@@ -24,11 +24,14 @@
           (height . 65))))
 
 ;; Update window divider in terminal
+;; https://www.reddit.com/r/emacs/comments/3u0d0u/how_do_i_make_the_vertical_window_divider_more/
 (unless (display-graphic-p)
   (defun my-change-window-divider ()
-    (let ((display-table (or buffer-display-table standard-display-table)))
-      (set-display-table-slot display-table 5 ?│)
-      (set-window-display-table (selected-window) display-table)))
+    (ignore-errors
+      (let ((display-table (or buffer-display-table standard-display-table)))
+        (set-display-table-slot display-table 5 ?│)
+        ;; (set-window-display-table (selected-window) display-table)
+        )))
   (add-hook 'window-configuration-change-hook #'my-change-window-divider))
 
 (after! doom-modeline
