cbdc6fb4a76fd7bf2c24ea893ba7f050e72c3616
Ting Zhou <ztlevi.work@gmail.com>
2021-02-07 21:40:38 +0800

    prog: remove import-js

--- +prog.el
+++ +prog.el
@@ -50,15 +50,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-(use-package! import-js
-  :defer t
-  :init
-  (add-hook! (js2-mode rjsx-mode) (run-import-js))
-  (add-hook! (js2-mode rjsx-mode)
-    (add-hook 'after-save-hook #'import-js-fix nil t)))
-(advice-add '+javascript|cleanup-tide-processes :after 'kill-import-js)
-
-
 (after! web-mode
   (web-mode-toggle-current-element-highlight)
   (web-mode-dom-errors-show))
