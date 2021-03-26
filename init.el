6b13faa6d4918dc4761d3509cc0b767d798e0982
Ting Zhou <ztlevi.work@gmail.com>
2021-03-26 19:29:36 +0800

    init: flycheck disable childframe

--- init.el
+++ init.el
@@ -89,8 +89,7 @@
        ;;vterm             ; the best terminal emulation in Emacs
 
        :checkers
-       (syntax
-        +childframe)     ; tasing you for every semicolon you forget
+       syntax            ; tasing you for every semicolon you forget
        (spell
         +aspell)         ; tasing you for misspelling mispelling
        grammar           ; tasing grammar mistake every you make
