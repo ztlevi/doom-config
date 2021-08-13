259cc1f6caa11b3c3c74c72be489c745b98aa1a6
Ting Zhou <ztlevi.work@gmail.com>
2021-08-13 08:44:07 +0800

    local config use true filepath

--- config.el
+++ config.el
@@ -50,5 +50,6 @@
 ;; Load system profile for different machines and work config
 (dolist (config '("~/.config/doom/local.el"
                   "~/work/dots/local.el"))
-  (when (file-exists-p config)
-    (load-file config)))
+  (let ((config-file (file-truename config)))
+    (when (file-exists-p config-file)
+      (load-file config-file))))
