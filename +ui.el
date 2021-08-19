9af90b310f8d30284fb8287c1ae83c2dcba9bfee
Ting Zhou <ztlevi.work@gmail.com>
2021-08-19 16:15:12 +0800

    ui: minor

--- +ui.el
+++ +ui.el
@@ -92,7 +92,7 @@
   `(flycheck-error :underline (:style wave :color ,(doom-color 'red)))
   `(flycheck-info :underline (:style wave :color ,(doom-color 'green)))
   `(doom-modeline-debug-visual :background ,(doom-blend 'red 'base0 0.3))
-  `(mode-line :background ,(doom-blend 'blue 'base0  0.2))
+  `(mode-line :background ,(doom-blend 'dark-blue 'base0  0.2))
   `(mode-line-inactive :background ,(doom-color 'bg-alt))
   `(ein:cell-input-area :background ,(doom-blend 'red 'base0 0.15))
   `(ein:cell-input-prompt :background ,(doom-color 'red) :foreground ,(doom-color 'base0) :bold t))
