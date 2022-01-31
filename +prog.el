5a64e7c70951ac6744bdafb4fd680ba57f75d0fe
Ting Zhou <ztlevi.work@gmail.com>
2022-01-30 17:33:35 -0800

    prog: remove redundant bazel content

--- +prog.el
+++ +prog.el
@@ -37,15 +37,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package! bazel-mode
-  :defer t
-  :commands bazel-mode
-  :init
-  (add-to-list 'auto-mode-alist '("BUILD\\(\\.bazel\\)?\\'" . bazel-mode))
-  (add-to-list 'auto-mode-alist '("WORKSPACE\\'" . bazel-mode))
-  :config
-  ;; disable format-all becuase it doesn't sort BUILD list variables
-  (setq bazel-mode-buildifier-before-save t)
-  (appendq! +format-on-save-enabled-modes '(bazel-mode)))
+  :defer t)
 
 (add-to-list 'auto-mode-alist '("\\.inl\\'" . +cc-c-c++-objc-mode))
 (add-to-list 'auto-mode-alist '("\\.inc\\'" . +cc-c-c++-objc-mode))
