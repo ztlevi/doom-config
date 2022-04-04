16312df5e78faec746b1bc03e7e1cef05660a91a
Ting Zhou <ztlevi.work@gmail.com>
2022-04-04 18:42:10 +0000

    disable emoji layer

--- init.el
+++ init.el
@@ -36,7 +36,7 @@
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       (emoji +ascii +github +unicode)  ; 🙂
+       ;; (emoji +ascii +github +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        indent-guides     ; highlighted indent columns
