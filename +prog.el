7c98fda5616b011f4630320c1e29b8d031aed9e3
Ting Zhou <ztlevi.work@gmail.com>
2022-04-19 21:45:56 +0000

    prog: custom lsp-imenu

--- +prog.el
+++ +prog.el
@@ -111,7 +111,10 @@
   (setq lsp-log-io nil
         lsp-file-watch-threshold 4000
         lsp-headerline-breadcrumb-enable t
-        lsp-headerline-breadcrumb-segments '(file symbols))
+        lsp-headerline-breadcrumb-segments '(file symbols)
+        lsp-imenu-index-symbol-kinds '(File Module Namespace Package Class Method Enum Interface
+                                            Function Variable Constant Struct Event Operator TypeParameter)
+        )
   (dolist (dir '("[/\\\\]\\.ccls-cache\\'"
                  "[/\\\\]\\.mypy_cache\\'"
                  "[/\\\\]\\.pytest_cache\\'"
