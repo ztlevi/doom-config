c0fb9dd8579b5a2545a39989e45ccc6001942f5a
Ting Zhou <ztlevi.work@gmail.com>
2026-07-03 00:45:15 -0700

    update

--- init.el
+++ init.el
@@ -37,7 +37,7 @@
        :ui
        ;;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
-       doom-dashboard    ; a nifty splash screen for Emacs
+       dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;; (emoji +ascii +github +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
